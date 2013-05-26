5b622c0869bf0471211daa1241c1cabe51b83f41
povloid <t34box@gmail.com>
2013-05-26 16:04:10 +0600

    Упражнение 3-8 -> Выполнение и компиляция выражений первая рабочая версия

--- ep3/src/calc.erl
+++ ep3/src/calc.erl
@@ -7,41 +7,31 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([parse/1]).
+-export([parse/1,calc/1]).
 
 
+%% Упражнение 3-8 -> Выполнение и компиляция выражений
 
+calc(Exp) -> calculate(parse(Exp)).
 
 parse(Exp) -> iparse({},clean(Exp)).
 
 
 
 %% ((2+3)-4) 
-%% iparse([]) -> [];
-%% iparse([$(|T])-> {iparse(T)};
-%% iparse([$+|[$(|T]])-> {iparse(T)};
-%% iparse([$-|[$(|T]])-> {minus,0,iparse(T)};
-%% 
-%% iparse([$)|T]) -> iparse(T);
-%% 
-%% iparse([X|[$+|T]] )->{plus, iparse(X), iparse(T)};
-%% iparse([X|[$-|T]])->{minus, iparse(X), iparse(T)};
-%% 
-%% iparse([H|T]) -> H.
-
 
 iparse(Tuple, []) -> Tuple;
 iparse(Tuple, [$(|T]) -> 
 	{H, TL} = iparse({},T),
 	iparse(erlang:append_element(Tuple, H), TL);
 
-iparse(Tuple, [$+|[$(|T]]) -> 
-	{H, TL} = iparse({},T),
-	iparse(erlang:append_element(Tuple, H), TL);
-
-iparse(Tuple, [$-|[$(|T]]) -> 
-	{H, TL} = iparse({},T),
-	iparse(erlang:append_element(Tuple, {0,minus,H}), TL);
+%% iparse(Tuple, [$+|[$(|T]]) -> 
+%% 	{H, TL} = iparse({},T),
+%% 	iparse(erlang:append_element(Tuple, H), TL);
+%% 
+%% iparse(Tuple, [$-|[$(|T]]) -> 
+%% 	{H, TL} = iparse({},T),
+%% 	iparse(erlang:append_element(Tuple, {0,minus,H}), TL);
 
 iparse(Tuple, [$)|T]) -> {Tuple, T};
 
@@ -49,10 +39,30 @@ iparse(Tuple, [$+|T]) ->
 	iparse(erlang:append_element(Tuple, plus),T);
 iparse(Tuple, [$-|T]) -> 
 	iparse(erlang:append_element(Tuple, minus),T);
+iparse(Tuple, [$*|T]) -> 
+	iparse(erlang:append_element(Tuple, mul),T);
+iparse(Tuple, [$/|T]) -> 
+	iparse(erlang:append_element(Tuple, divade),T);
+
 
 iparse(Tuple, [H|T]) -> 
 	{Hi,_} = string:to_integer([H]),
 	iparse(erlang:append_element(Tuple, {num,Hi}),T).
+
+
+
+calculate({}) -> 0;
+calculate({num,X}) -> X;
+
+calculate({minus,X}) -> -1 * calculate(X);
+
+calculate({X,plus,Y}) -> calculate(X) + calculate(Y);
+calculate({X,minus,Y}) -> calculate(X) - calculate(Y);
+calculate({X,mul,Y}) -> calculate(X) * calculate(Y);
+calculate({X,divade,Y}) -> calculate(X) / calculate(Y);
+calculate(_) -> "syntax error".
+
+
 
 
 
