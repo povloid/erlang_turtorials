6a4dc921bf390ba10931ec77078270a554ac087b
povloid <t34box@gmail.com>
2013-05-27 19:47:17 +0600

    Упражнение 3-9 -> Индексирование Выполнено!

--- ep3/src/index.erl
+++ ep3/src/index.erl
@@ -2,13 +2,16 @@
 %% @doc @todo Add description to index.
 
 
+%% Упражнение 3-9 -> Индексирование
+
 -module(index).
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
 -export([text/0,read_file/0,clean/1,split_text/1,create_map/1
-		,split_text_to_string/1,create_strings_words_lists/1,create_index/2,index/0]).
+		,split_text_to_string/1,create_strings_words_lists/1,create_index/2,index/0, print/0,
+		 create_index_file/0,pac/4]).
 
 
 
@@ -16,11 +19,53 @@ index() ->
  	create_index(create_strings_words_lists(split_text_to_string(text())),
 			 create_map(split_text(text()))). 
 	
+create_index_file() ->
+	file:write_file("c:/erl/file.index", print()).
+
+
+print() ->
+	print_index_a("", index()).
+
+print_index_a(Strings, []) -> Strings;
+print_index_a(Strings, [X|T]) ->
+	{Word, Index} = X,
+	print_index_a(Strings ++ Word ++ "  " ++ pac("",0,0,lists:sort(Index)) ++ "\n" , T). 
 
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+  
+%% Примеры запуска: 
+%% 14> index:pac("", 0, 0, [1,3,5,6,7,10,11,12,14,15]).
+%% "1,3,5-7,10-12,14-15"
+%% 15> index:pac("", 0, 0, [1,3,5,6,7,10,11,12,14]).   
+%% "1,3,5-7,10-12,14"
+%% 16> index:pac("", 0, 0, [1,2,3,5,6,7,10,11,12,14]).
+%% "1-3,5-7,10-12,14"
+%% 17> index:pac("", 0, 0, [1,2,3,6,7,10,11,12,14]).  
+%% "1-3,6-7,10-12,14"
+%% 18> index:pac("", 0, 0, [1,2,4,6,7,10,11,12,14]).
+%% "1-2,4,6-7,10-12,14"
+
+pac(String, 1 , X ,[]) -> String ++ integer_to_list(X);
+pac(String, _ , _ ,[]) -> String;
+pac(String, 0 , 0, [Y|T]) ->
+	pac( String ++ integer_to_list(Y), 0,  Y, T );
+pac(String, 0 , X ,[Y|T]) when ((X == Y) or ((X + 1) == Y)) ->
+	pac( String ++ "-" , 1 , X + 1, T);
+
+pac(String, 1 , X, [Y|T]) when (X + 1) == Y ->
+	pac( String , 1 , X + 1, T);
+pac(String, 1 , X, [Y|T]) when X == Y ->
+	pac( String , 1 , X, T);
+
+pac(String, 0,  _, [Y|T]) -> 
+	pac( String ++ "," ++ integer_to_list(Y) , 0,  Y, T);
+pac(String, _,  X, [Y|T]) -> 
+	pac( String ++ integer_to_list(X) ++ "," ++ integer_to_list(Y) , 0,  Y, T).
+
+
 
 create_index(Strings, Index) -> create_index_a(1,Strings, Index).
 
